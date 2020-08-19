61488a61b10060ca7001feb857f10f2279fcb590
Yeh Swee Khim <sweekhim996@gmail.com>
2020-08-20 02:35:31 +0800

    Dangerous obstacles (Fire and Potholes)

---
+++ Assets/Scripts/Canvas/Timer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    public Image LoadingBar;
+    public Text timeText;
+    public int min;
+    public int sec;
+    int totalsec = 0;
+    int total = 0;
+
+    private void Start () {
+        timeText.text = min + ":" + sec.ToString ("D2");
+        if (min > 0) totalsec += min * 60;
+        if (sec > 0) totalsec += sec;
+        total = totalsec;
+        StartCoroutine (second ());
+    }
+
+    private void Update () {
+        if (sec == 0 && min == 0) {
+            StopCoroutine (second ());
+        }
+    }
+
+    IEnumerator second () {
+        yield return new WaitForSeconds (1f);
+        if (sec > 0) sec--;
+        if (sec == 0 && min != 0) {
+            sec = 60;
+            min--;
+        }
+        timeText.text = min + ":" + sec.ToString ("D2");
+        fillLoadingBar ();
+        StartCoroutine (second ());
+    }
+
+    private void fillLoadingBar () {
+        totalsec--;
+        float fill = (float) totalsec / total;
+        LoadingBar.fillAmount = fill;
+    }
+}
---
+++ Assets/Scripts/Obstacles/Obstacles.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour {
+
+    public GameObject timer;
+    Timer time;
+    bool check = false;
+    public GameObject fire;
+    public Transform fireObstacles;
+    public GameObject tile;
+    public Transform tileObstacles;
+    GameObject[] spawnPoints;
+    List<GameObject> spawnLeft;
+
+    void Start () {
+        time = timer.GetComponent<Timer> ();
+        spawnPoints = GameObject.FindGameObjectsWithTag ("SpawnPoint");
+        spawnLeft = new List<GameObject> (spawnPoints);
+        InvokeRepeating ("spawnFire", 0f, 5f);
+        InvokeRepeating ("spawnPothole", 0f, 5f);
+        InvokeRepeating ("spawnTile", 0f, 2f);
+    }
+
+    void Update () {
+        if (time.sec == 10 && !check) {
+            CancelInvoke ("spawnTile");
+            check = true;
+            float countdown = 10f / spawnLeft.Count;
+            Debug.Log (countdown);
+            InvokeRepeating ("spawnTile", 0f, countdown);
+        }
+    }
+
+    private void spawnFire () {
+        int destination = chooseDestination (spawnPoints.Length);
+        spawn (fire, spawnPoints[destination].transform.position, fireObstacles);
+    }
+
+    private void spawnTile () {
+        int destination = chooseDestination (spawnLeft.Count);
+        spawn (tile, spawnLeft[destination].transform.position, tileObstacles);
+        spawnLeft.RemoveAt (destination);
+    }
+
+    private void spawnPothole () {
+        StartCoroutine (choosePothole ());
+    }
+
+    private int chooseDestination (int length) {
+        return Random.Range (0, length);
+    }
+
+    private void spawn (GameObject obj, Vector3 pos, Transform parent) {
+        GameObject clone = Instantiate (obj, pos, Quaternion.Euler (0, 180, 0));
+        clone.transform.SetParent (parent);
+    }
+
+    private IEnumerator choosePothole () {
+        int destination = chooseDestination (spawnPoints.Length);
+        spawnPoints[destination].SetActive (false);
+        yield return new WaitForSeconds (2);
+        spawnPoints[destination].SetActive (true);
+    }
+
+}
