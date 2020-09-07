8780db4f6f2b7f30ed7778996341c7dfe68499e1
Yeh Swee Khim <sweekhim996@gmail.com>
2020-09-07 21:11:44 +0800

    Character selection screen and Start screen

---
+++ Assets/Scripts/Character Selection/OnTouch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OnTouch : MonoBehaviour, IPointerDownHandler {
+
+    // Vector3 touchPosWorld;
+    // TouchPhase touchPhase = TouchPhase.Ended;
+    Transform currentCharacter;
+
+    bool isBeingHeld;
+
+    public void OnPointerDown (PointerEventData eventData) {
+        currentCharacter = eventData.pointerEnter.gameObject.transform.parent;
+        print (currentCharacter.name);
+        if (currentCharacter != null) {
+            int index = currentCharacter.GetSiblingIndex();
+            Character character = SpawnCharacter.instance.characters[index];
+            SpawnCharacter.instance.ShowCharacterInSlot(0, character);
+        } else {
+            SpawnCharacter.instance.ShowCharacterInSlot(0, null);
+        }
+    }
+
+    // Screen Touch
+    // void Update () {
+    //     if (Input.touchCount > 0 && Input.GetTouch (0).phase == touchPhase) {
+    //         touchPosWorld = Camera.main.ScreenToWorldPoint (Input.GetTouch (0).position);
+    //         Vector2 touchPosWorld2D = new Vector2 (touchPosWorld.x, touchPosWorld.y);
+    //         RaycastHit2D hitInfo = Physics2D.Raycast (touchPosWorld2D, Camera.main.transform.forward);
+    //         if (hitInfo.collider != null) {
+    //             GameObject touchedObject = hitInfo.transform.gameObject;
+    //             Debug.Log ("Touched " + touchedObject.transform.name);
+    //         }
+    //     }
+    // }
+}
---
+++ Assets/Scripts/Character Selection/SpawnCharacter.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnCharacter : MonoBehaviour {
+    public static SpawnCharacter instance;
+    public List<Character> characters = new List<Character> ();
+    public GameObject charCellPrefab;
+    public Transform playerSlotsContainer;
+
+    void Awake () {
+        instance = this;
+    }
+
+    void Start () {
+        foreach (Character character in characters) {
+            SpawnCharacterCell (character);
+        }
+    }
+
+    private void SpawnCharacterCell (Character character) {
+        GameObject charCell = Instantiate (charCellPrefab, transform);
+
+        Image artwork = charCell.transform.Find ("Image").GetComponent<Image> ();
+        Text name = charCell.transform.Find ("TextBG").GetComponentInChildren<Text> ();
+
+        artwork.sprite = character.characterSprite;
+        name.text = character.characterName;
+        charCell.name = name.text;
+
+        Vector2 pixelSize = new Vector2 (artwork.sprite.texture.width, artwork.sprite.texture.height);
+        Vector2 pixelPivot = artwork.sprite.pivot;
+        Vector2 uiPivot = new Vector2 (pixelPivot.x / pixelSize.x, pixelPivot.y / pixelSize.y);
+
+        artwork.GetComponent<RectTransform> ().pivot = uiPivot;
+        artwork.GetComponent<RectTransform> ().sizeDelta *= character.zoom;
+    }
+
+    public void ShowCharacterInSlot (int player, Character character) {
+        bool nullChar = (character == null);
+        Sprite artwork = nullChar? null : character.characterSprite;
+        string name = nullChar? string.Empty : character.characterName;
+        string playerNickname = nullChar? string.Empty: "Player " + player.ToString ();
+        string playerNum = nullChar? string.Empty: "P" + player.ToString ();
+
+        Transform slot = playerSlotsContainer.GetChild (player);
+        slot.Find ("Image").GetComponent<Image> ().sprite = artwork;
+        slot.Find ("CharName").GetComponent<Text> ().text = name;
+        slot.Find ("PlayerName").GetComponentInChildren<Text> ().text = playerNickname;
+    }
+}
